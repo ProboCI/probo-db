2acf2c16569fdeb6f2d5470aab60e42d8bfeadf4
Michael Bagnall <mrbagnall@icloud.com>
2021-04-06 15:39:47 -0400

    Work on RabbitMQ integration

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [ ! -e "/etc/probo/db-initialized" ]; then
-  exec /home/probo/app/bin/migrate -c /etc/probo/db.yaml -k /etc/probo/knexfile.js
-  touch "/etc/probo/db-initialized"
-fi
+#if [ ! -e "/etc/probo/db-initialized" ]; then
+#  /home/probo/app/bin/migrate -c /etc/probo/db.yaml -k /etc/probo/knexfile.js
+#  touch "/etc/probo/db-initialized"
+#fi
 
-exec /home/probo/app/bin/probo-db -c /etc/probo/db.yaml -k /etc/probo/knexfile.js
+/home/probo/app/bin/startup
+/home/probo/app/bin/probo-db -c /etc/probo/db.yaml -k /etc/probo/knexfile.js
