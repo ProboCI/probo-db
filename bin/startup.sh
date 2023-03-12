0e1cae5d7ca75a620e9b6f450fceb8ff44e21050
Michael Bagnall <michael@bagnall.io>
2023-03-12 15:43:11 -0500

    Change paths - make sure we're using sh and not bash

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ ! -e "/etc/probo/db-initialized" ]; then
-  /home/probo/app/bin/migrate -c /etc/probo/db.yaml -k /etc/probo/knexfile.js
+  sh ./bin/migrate -c /etc/probo/db.yaml -k /etc/probo/knexfile.js
   touch "/etc/probo/db-initialized"
 fi
 
-exec /home/probo/app/bin/probo-db -c /etc/probo/db.yaml -k /etc/probo/knexfile.js
+exec sh ./bin/probo-db -c /etc/probo/db.yaml -k /etc/probo/knexfile.js
